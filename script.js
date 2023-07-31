7f1bb45cec710dae03676147c07e2d9c72dfe82c
norizmi <naufalnaufal841@gmail.com>
2023-07-31 17:16:33 +0700

    fizzbuzz not fixed

---
+++ script.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector("button");
+let result = document.getElementById("result");
+
+
+btn.addEventListener("click", () => {
+  const number = document.querySelector("input").value;
+  const li = document.createElement("li");
+    if (number % 3 === 0 && number % 5 === 0) {
+        li.textContent = "Fizz Buzz";
+      } else if (number % 3 === 0) {
+        li.textContent = "Fizz";
+      } else if (number % 5 === 0) {
+        li.textContent = "Buzz";
+      } else if(isNaN(number)) {
+        li.setAttribute("style", "color:red; margin-top:20px");
+        li.innerHTML = 'invalid input';
+      } else if(number) {
+        li.textContent = `${number}`;
+      }
+      result.appendChild(li);
+});
